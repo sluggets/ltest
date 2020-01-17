985bddfe883ae6cf42c873f41122455155a136a3
timothy <tim.longoria@gmail.com>
2020-01-17 07:36:52 -0500

    Test views added

---
+++ resources/views/name.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv='x-ua-compatible' content='ie=edge'>
+    <meta name='description' content=''>
+    <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+    <link rel='stylesheet' href='/main.css'>
+    <script src='/main.js'></script>
+    <meta charset="UTF-8">
+    <title>Test Title</title>
+  </head>
+  <body>
+    <header>Test Website Header for {{ $name }}</header>
+    <nav>
+      <ul>
+        <li><a href="/about">About Page</a></li>
+        <li><a href="/contact">Contact Us</a></li>
+        <li><a href="/">Home</a></li>
+      </ul>
+    </nav>
+  </body>
+</html>
